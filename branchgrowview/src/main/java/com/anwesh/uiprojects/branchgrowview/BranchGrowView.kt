80412ee0be47fcd0b7d379b3e928592790178e9a
Anwesh43 <anweshthecool0@gmail.com>
2019-10-13 11:27:18 +0530

    adding constants and libraries

--- branchgrowview/src/main/java/com/anwesh/uiprojects/branchgrowview/BranchGrowView.kt
+++ branchgrowview/src/main/java/com/anwesh/uiprojects/branchgrowview/BranchGrowView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.branchgrowview
 /**
  * Created by anweshmishra on 13/10/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val circleSizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#00C853")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
