ec7b3a36417850eac1708c0cf60eb3ac6f01bd9b
Anwesh43 <anweshthecool0@gmail.com>
2019-10-13 11:45:12 +0530

    adding Canvas extension methods

--- branchgrowview/src/main/java/com/anwesh/uiprojects/branchgrowview/BranchGrowView.kt
+++ branchgrowview/src/main/java/com/anwesh/uiprojects/branchgrowview/BranchGrowView.kt
@@ -23,3 +23,31 @@ val delay : Long = 20
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBranchGrow(sf : Float, size : Float, sc : Float, paint : Paint) {
+    val sc1 : Float = sc.divideScale(0, 2)
+    val sc2 : Float = sc.divideScale(1, 2)
+    val r : Float = size / circleSizeFactor
+    save()
+    translate(0f, size / 2)
+    drawLine(0f, 0f, size * sf * sc1, 0f, paint)
+    save()
+    translate(0f, size * sf)
+    drawCircle(0f, 0f, r * sc2, paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawBGNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / nodes
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(w / 2, gap * i)
+    drawLine(0f, 0f, 0f, gap + gap * scale, paint)
+    drawBranchGrow(1f - 2 * (i % 2), gap, scale, paint)
+    restore()
+}
